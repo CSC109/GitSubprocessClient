1899f6049955a3dea648353e1edb91d122b3cf98
a-r-t <alexthimineur@gmail.com>
2021-04-06 19:51:07 -0400

    removed the println statement

--- src/git/tools/client/GitSubprocessClient.java
+++ src/git/tools/client/GitSubprocessClient.java
@@ -44,7 +44,6 @@ public class GitSubprocessClient {
 
     public String gitCommit(String message) {
         String command = String.format("git commit -m \"%s\"", message);
-        System.out.println(command);
         return Subprocess.runCommand(command, repoPath);
     }
 
